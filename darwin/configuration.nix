b53c4e3d0d548fdd3662a2e91bd50f0cdfe71a0c
Thomas Maurice <thomas@maurice.fr>
2025-11-04 02:12:35 +0100

    qtwebengine

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -9,6 +9,9 @@
 
   nixpkgs.config.allowUnsupportedSystem = true;
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.permittedInsecurePackages = [
+    "qtwebengine-5.15.19"
+  ];
 
   system.primaryUser = "thomas";
 
