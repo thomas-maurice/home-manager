45d8653f8d3adbc796f6fe6eff2a13ef2bff83be
Thomas Maurice <thomas@maurice.fr>
2025-11-04 02:26:25 +0100

    trackpad

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -37,32 +37,16 @@
   '';
 
   # macOS system preferences
-  # system.defaults = {
-  #   dock = {
-  #     autohide = true;
-  #     show-recents = false;
-  #     tilesize = 48;
-  #     mru-spaces = false;
-  #   };
-  #
-  #   finder = {
-  #     AppleShowAllExtensions = true;
-  #     FXEnableExtensionChangeWarning = false;
-  #     ShowPathbar = true;
-  #     ShowStatusBar = true;
-  #   };
-  #
-  #   NSGlobalDomain = {
-  #     AppleShowAllExtensions = true;
-  #     InitialKeyRepeat = 15;
-  #     KeyRepeat = 2;
-  #     "com.apple.swipescrolldirection" = false; # Disable natural scrolling
-  #   };
-  #
-  #   trackpad = {
-  #     Clicking = true; # Enable tap to click
-  #   };
-  # };
+  system.defaults = {
+    NSGlobalDomain = {
+      AppleInterfaceStyle = "Dark"; # Enable dark mode
+      "com.apple.swipescrolldirection" = false; # Disable natural scrolling (reverse scroll direction)
+    };
+
+    trackpad = {
+      Clicking = true; # Enable tap to click
+    };
+  };
 
   # Enable Touch ID for sudo
   # security.pam.enableSudoTouchIdAuth = true;
