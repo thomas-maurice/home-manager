60a382330e33eda2fee200b63b53ba8ab9d5f03f
Thomas Maurice <thomas@maurice.fr>
2025-11-29 22:56:31 +0100

    Fix deprecation warnings

--- modules/packages/git/default.nix
+++ modules/packages/git/default.nix
@@ -10,15 +10,17 @@
   programs.git = {
     enable = true;
 
-    userName = "Thomas Maurice";
-    userEmail = "thomas@maurice.fr";
+    settings = {
+      user = {
+        name = "Thomas Maurice";
+        email = "thomas@maurice.fr";
+      };
 
-    signing = lib.mkIf (signingKey != null) {
-      key = signingKey;
-      signByDefault = true;
-    };
+      signing = lib.mkIf (signingKey != null) {
+        key = signingKey;
+        signByDefault = true;
+      };
 
-    extraConfig = {
       push = {
         default = "simple";
       };
@@ -51,33 +53,33 @@
       url."ssh://gitea@gitea.plil.fr/" = {
         insteadOf = "https://gitea.plil.fr/";
       };
-    };
 
-    aliases = {
-      ci = "commit";
-      co = "checkout";
-      cob = "checkout -b";
-      bs = "!git for-each-ref --sort='-authordate' --format='%(authordate)%09%(objectname:short)%09%(refname)' refs/heads | sed -e 's-refs/heads/--'";
-      br = "branch";
-      rbi = "!git rebase -i $(git rev-list --first-parent master | head -1)";
-      st = "status";
-      am = "commit --amend";
-      d = "diff";
-      ds = "diff --stat";
-      dc = "diff --cached";
-      gr = "log --pretty=oneline --abbrev-commit --graph";
-      lc = "!git gr ORIG_HEAD.. --stat --no-merges";
-      lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
-      f = "fetch -p";
-      ba = "branch -a";
-      rank = "shortlog -sn --no-merges";
-      sweep = "!git branch --merged | egrep -v '(^\\*|master|devel|stag|prod)' | xargs git branch -d";
-      undo = "reset --soft HEAD^";
-      alias = "!git config -l | grep ^alias | cut -c 7- | sort";
-      amend = "commit --amend";
-      pullr = "!BRANCH=$(git branch | grep \\* | cut -d ' ' -f2); git pull --rebase origin $BRANCH";
-      pushdev = "!BRANCH=$(git branch | grep \\* | cut -d ' ' -f2); git push origin $BRANCH";
-      pushdevf = "!BRANCH=$(git branch | grep \\* | cut -d ' ' -f2); git push -f origin $BRANCH";
+      alias = {
+        ci = "commit";
+        co = "checkout";
+        cob = "checkout -b";
+        bs = "!git for-each-ref --sort='-authordate' --format='%(authordate)%09%(objectname:short)%09%(refname)' refs/heads | sed -e 's-refs/heads/--'";
+        br = "branch";
+        rbi = "!git rebase -i $(git rev-list --first-parent master | head -1)";
+        st = "status";
+        am = "commit --amend";
+        d = "diff";
+        ds = "diff --stat";
+        dc = "diff --cached";
+        gr = "log --pretty=oneline --abbrev-commit --graph";
+        lc = "!git gr ORIG_HEAD.. --stat --no-merges";
+        lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+        f = "fetch -p";
+        ba = "branch -a";
+        rank = "shortlog -sn --no-merges";
+        sweep = "!git branch --merged | egrep -v '(^\\*|master|devel|stag|prod)' | xargs git branch -d";
+        undo = "reset --soft HEAD^";
+        alias = "!git config -l | grep ^alias | cut -c 7- | sort";
+        amend = "commit --amend";
+        pullr = "!BRANCH=$(git branch | grep \\* | cut -d ' ' -f2); git pull --rebase origin $BRANCH";
+        pushdev = "!BRANCH=$(git branch | grep \\* | cut -d ' ' -f2); git push origin $BRANCH";
+        pushdevf = "!BRANCH=$(git branch | grep \\* | cut -d ' ' -f2); git push -f origin $BRANCH";
+      };
     };
 
     ignores = [
